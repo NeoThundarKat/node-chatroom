b46dc7e9ddb4cb453fdbec6ac2416551bd59aa1a
NeoThundarKat <thundarkat@gmail.com>
2015-12-22 20:39:18 +0000

    ACTUAL INIT THIS TIME

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+﻿var socket = io();
+$('#send-message-btn').click(function () {
+    var msg = {
+        content: $('#message-box').val(),
+        user: $('#user-box').val()
+    };
+    socket.emit('chat', msg);
+    $('#messages').append($('<p>').html("<span style='color:#2d6ca2'>" + msg.user + "</span>: " + msg.content));
+    $('#message-box').val('');
+    console.log(msg);
+    return false;
+});
+socket.on('chat', function (msg) {
+    $('#messages').append($('<p>').html("<span style='color:#2d6ca2'>" + msg.user + "</span>: " + msg.content));
+    console.log(msg);
+});
